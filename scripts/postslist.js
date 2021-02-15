4f120b00084e04a1892c0d09153655d42e3010be
Rakesh Baravathaya Badaje <baravathaya@gmail.com>
2021-02-14 20:10:48 -0500

    Implement Post page

---
+++ scripts/postslist.js
@@ -0,0 +1,34 @@
+var postSelected;
+
+//Save the contents of the post before redirecting
+redirectPost = () => {
+    var author = postSelected.querySelector(".author p").innerText.trim();
+    var title = postSelected.querySelector(".title p").innerText.trim();
+    var content = postSelected.querySelector(".content").innerText.trim();
+
+    sessionStorage.setItem('author', author);
+    sessionStorage.setItem('title', title);
+    sessionStorage.setItem('content', content);
+
+    window.location.href = '../html/post.html'
+};
+
+//Delete the Post
+deletePost = () => {
+    postSelected.remove();
+};
+
+//Get the Parent Div for the selected post
+getPostData = (e) => {
+    if (e.target.classList.contains('fa-trash')) {
+        postSelected = (e.target.parentElement.parentElement.parentElement.parentElement);
+    }
+
+    if (e.target.classList.contains('fa-ellipsis-h')) {
+        postSelected = (e.target.parentElement.parentElement.parentElement);
+        redirectPost();
+    }
+};
+
+//Event Listener
+document.getElementById("postContainer").addEventListener("click", getPostData);
